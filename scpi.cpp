3a0c0a8a831d44e88ff69af3e829fffbe2a688e0
b1f6c1c4 <b1f6c1c4@gmail.com>
2022-05-10 00:23:17 -0400

    create repo

---
+++ scpi.cpp
@@ -0,0 +1,73 @@
+#include "scpi.hpp"
+
+#include <arpa/inet.h>
+#include <cerrno>
+#include <cstring>
+#include <fcntl.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std::string_literals;
+
+scpi::scpi(int fd) :
+    _fd{ fd },
+    _fb{ fd, std::ios::in | std::ios::out },
+    _fs{ &_fb } { }
+
+void scpi::send(std::string s) {
+    _fs << s << std::endl;
+}
+
+scpi::~scpi() {
+    _fb.close();
+}
+
+std::string scpi::recv() {
+    std::string str;
+    std::getline(_fs, str);
+    return str;
+}
+
+double scpi::recv_number() {
+    double n;
+    _fs >> n;
+    while (_fs.get() != '\n');
+    return n;
+}
+
+scpi_tcp::scpi_tcp(const std::string &host, int port) :
+    scpi{ [&]{
+        int sfd{ socket(AF_INET, SOCK_STREAM, 0) };
+        if (sfd < 0)
+            throw std::runtime_error{ "socket:"s + std::strerror(errno) };
+        static constexpr const addrinfo hint{
+            .ai_family = AF_INET,
+            .ai_socktype = SOCK_STREAM,
+            .ai_protocol = 0,
+        };
+        addrinfo *ai;
+        if (auto ret = getaddrinfo(host.data(), nullptr, &hint, &ai); ret)
+            throw std::runtime_error{ "getaddrinfo:"s + gai_strerror(ret) };
+        for (auto ptr = ai; ptr; ptr = ptr->ai_next)
+            if (auto ret = connect(sfd, ptr->ai_addr, ptr->ai_addrlen); !ret) {
+                freeaddrinfo(ai);
+                return sfd;
+            }
+        freeaddrinfo(ai);
+        throw std::runtime_error{ "Cannot connect to " + host };
+    }() } { }
+
+scpi_tty::scpi_tty(const std::string &dev) :
+    scpi{ [&]{
+        auto fd = open(dev.data(), O_RDWR | O_NOCTTY);
+        if (fd < 0)
+            throw std::runtime_error{ "open:"s + std::strerror(errno) };
+        return fd;
+    }() } {
+    int flag = TIOCM_DTR;
+    if (ioctl(_fd, TIOCMBIC, &flag) == -1)
+        throw std::runtime_error{ "ioctl:"s + std::strerror(errno) };
+}
---
+++ scpi.hpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <fstream>
+#include <ext/stdio_filebuf.h>
+
+class scpi {
+protected:
+    int _fd;
+    __gnu_cxx::stdio_filebuf<char> _fb;
+    std::iostream _fs;
+
+    scpi(int fd);
+    virtual ~scpi();
+
+public:
+    void send(std::string s);
+    std::string recv();
+    double recv_number();
+};
+
+class scpi_tcp : public scpi {
+public:
+    scpi_tcp(const std::string &host, int port);
+};
+
+class scpi_tty : public scpi {
+public:
+    scpi_tty(const std::string &dev);
+};
